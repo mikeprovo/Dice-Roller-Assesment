6d9abcf6e0490c391934a67deb610cf302b59803
Mike Provo <mikeprovo34@gmail.com>
2020-05-15 16:17:34 -0500

    updated some of the java

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let dieRoll = []
+var dieRoll = []
 
-addEventListener("click")
+let showAllButton = document.querySelector(".Button-2")
+
+let rollButton = document.querySelector(".Button-1")
+
+rollButton.addEventListener("click", function) () {
+    numRolled=document.getElementById("numRolled").value
+    dieRoll=[]
+
+    let x=0
+        while (x < numRolled.value) {
+            x++
+            var resultForEachDice = 0;
+            resultForEachDice = Math.floor((Math.random() * 6) + 1)
+            dieRoll.push(resultForEachDice);
+        }
+}
