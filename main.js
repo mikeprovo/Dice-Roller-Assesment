fda0d994e2a6cfbb0e3598cb51dd9a23186279c9
Mike Provo <mikeprovo34@gmail.com>
2020-05-20 00:01:59 -0500

    changes were made lol

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ let showAllButton = document.querySelector(".Button-2")
 
 let rollButton = document.querySelector(".Button-1")
 
+let rolls = 0
+
 rollButton.addEventListener("click", function) () {
     numRolled=document.getElementById("numRolled").value
     dieRoll=[]
-
+    rolls=0
     let x=0
         while (x < numRolled.value) {
             x++
@@ -15,4 +17,12 @@ rollButton.addEventListener("click", function) () {
             resultForEachDice = Math.floor((Math.random() * 6) + 1)
             dieRoll.push(resultForEachDice);
         }
+
+
+}
+showAllButton.addEventListener("click", function) (){
+
+let numberOfRolls=dieRolls.length, roll,i;
+    for (i = 0; i < numberOfRolls; ++i) {  
+    }
 }
